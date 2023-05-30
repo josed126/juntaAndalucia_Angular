1303afbc2d8648d6c9a3047957881e04d5494298
Jose David Lopez Herrera <jdlopezh@ayesa.es>
2023-05-30 09:52:42 +0200

    Primera version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RedesComponent } from './redes/redes.component';
+import { NavegadorComponent } from './navegador/navegador.component';
+import { BuscadorComponent } from './buscador/buscador.component';
+import { EnlacesComponent } from './enlaces/enlaces.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RedesComponent,
+    NavegadorComponent,
+    BuscadorComponent,
+    EnlacesComponent
   ],
   imports: [
     BrowserModule,
